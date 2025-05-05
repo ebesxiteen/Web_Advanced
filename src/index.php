8d3809a6da75a92285f08dcc9b0bd9eeaee3b5aa
ebesxiteen <152803029+ebesxiteen@users.noreply.github.com>
2025-05-05 13:51:16 +0700

    ok

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
     // require_once 'controllers/AccountController.php';
-    require_once(__DIR__ . '/../../../config/configUrl.php');
+    require_once(__DIR__ . '/config/configUrl.php');
     include("./views/Pages/Home.php");
 ?>
